afd0ef64fcf00fa21becc5ca36233de5442b9ae9
niki-shan <nikitawaghmare96@gmail.com>
2023-07-19 12:39:06 +0530

    change in service

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,24 @@
 const cl=console.log
 
-$('.tmslider').owlCarousel()
+// $('.tmslider').owlCarousel({
+//     nav:true,
+    
+// })
+
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
